84c538b8317be05082674ed211ce29b21c3daa40
MARKOS69 <58507819+MARKOS69@users.noreply.github.com>
2019-12-04 13:16:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const discord = require("discord.js");
+const Discord = require("discord.js");
 const fs = require("fs");
 const botconfig = require("./botconfig.json");
 const got = require('got');
